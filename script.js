f2d39659064b4cdf5ac7966181b51d070f01bedb
raqeeb838 <raqeebaptech@gmail.com>
2024-12-04 22:43:14 +0500

    working on it

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-Console.log("Abdul Raqeeb");
 const raqeeb = "korejo";
+let accountEmail = "raqeeb78@gmail.com";
+var accountPassword = "7777777";
+accountCity = "Karachi";
+let hamza;
 
-console.log(raqeeb);
+// raqeeb = "korejo";
+accountEmail = "memon@gmail.com";
+accountPassword = "8888";
+accountCity = "lahore";
+hamza;
+
+console.table([raqeeb, accountEmail, accountPassword, accountCity, hamza]);
